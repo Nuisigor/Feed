604b026f6014a34da5ccdd1acf3a35445f495450
Igor Lara de Oliveira <igooli@alunos.utfpr.edu.br>
2022-08-30 23:13:04 +0000

    Mudando o estilo do post

--- feed/src/Components/Posts/PostsList.tsx
+++ feed/src/Components/Posts/PostsList.tsx
@@ -1,44 +1,41 @@
-import { useEffect, useState } from "react"
-import { post } from "../../data/@types/Post"
-import ModalImage from "react-modal-image"
-import "./PostList.css"
+import { useEffect, useState } from "react";
+import { post } from "../../data/@types/Post";
+import ModalImage from "react-modal-image";
+import "./PostList.css";
+import { Paper } from "@mui/material";
 
-interface Posts{
-    posts : post[],
+interface Posts {
+  posts: post[];
 }
 
-
-
-export default function PostList(props: Posts){
-
-
-    return (
-        <>
-            {props.posts.map( (postData) => 
-                <div key={postData.id} class="Post">
-                    <div class="Info">
-                        <div class="Infoimg">
-                            <img src="https://avatars.githubusercontent.com/u/50688059?v=4"></img>
-                        </div>
-                        <div class="Infodata">
-                            <p class="autor">{postData.autor}</p>
-                            <p class="date">{postData.date}</p>
-                        </div>
-                    </div>
-                    <p class="Data">{postData.data}</p>
-                    <div class="PostImage">
-                        {postData.image.map( (image) => 
-                            <ModalImage className="Image"
-                                hideZoom={true}
-                                hideDownload={true}
-                                small={image}
-                                large={image}
-                            />
-                        )}
-                    </div>
-                </div>
-            )}
-        </>
-    )
-
+export default function PostList(props: Posts) {
+  return (
+    <>
+      {props.posts.map((postData) => (
+        <Paper elevation={6} variant="outlined" className="Post">
+          <div class="Info">
+            <div class="Infoimg">
+              <img src="https://avatars.githubusercontent.com/u/50688059?v=4"></img>
+            </div>
+            <div class="Infodata">
+              <p class="autor">{postData.autor}</p>
+              <p class="date">{postData.date}</p>
+            </div>
+          </div>
+          <p class="Data">{postData.data}</p>
+          <div class="PostImage">
+            {postData.image.map((image) => (
+              <ModalImage
+                className="Image"
+                hideZoom={true}
+                hideDownload={true}
+                small={image}
+                large={image}
+              />
+            ))}
+          </div>
+        </Paper>
+      ))}
+    </>
+  );
 }
