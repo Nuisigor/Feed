6ca1909f971cd93fcd5f7afd12bd08c90cc0c903
Igor Lara <dexchanel@gmail.com>
2022-08-31 18:19:56 -0300

    Arrumando ProfileMenu

--- feed/src/Components/ProfileMenu/ProfileMenu.tsx
+++ feed/src/Components/ProfileMenu/ProfileMenu.tsx
@@ -6,9 +6,11 @@ export default function ProfileMenu() {
     <div className="ProfileMenu">
       <Paper elevation={3} className="PaperMenu">
         <img src="https://avatars.githubusercontent.com/u/50688059?v=4" />
-        <a>Nome Nome Nome</a>
-        <a>Perfil</a>
-        <a>Curtidas</a>
+        <a className="Name">Nome Nome Nome</a>
+        <div className="Links">
+          <a>Perfil</a>
+          <a>Curtidas</a>
+        </div>
       </Paper>
     </div>
   );
